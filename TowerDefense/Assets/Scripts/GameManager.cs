e05e975cbee27a81a8b855665e5dab2ca1c05458
atapardea <atapardea@gmail.com>
2017-02-13 00:48:22 +0200

    added code for waypoints
    adde code for waypoints

---
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance=null;
+	public GameObject spawnPoint;
+	public GameObject[] enemies;
+	public int maxEnemiesOnScreen;
+	public int totalEnemies;
+	public int enemiesPerSpawin;
+
+	private int enemiesOnScreen=0;
+
+	void Awake(){
+		if (instance==null)
+			instance=this;
+		else if (instance != this) 
+			Destroy(gameObject);
+		
+		DontDestroyOnLoad(gameObject);	
+		}
+
+	
+	// Use this for initialization
+	void Start () {
+		spawnEnemy();
+	}
+	
+	// Update is called once per frame
+	
+
+	void spawnEnemy(){
+		if (enemiesPerSpawin>=0 && enemiesOnScreen < totalEnemies){
+			for (int i=0; i<enemiesPerSpawin ; i++){
+				if (enemiesOnScreen < maxEnemiesOnScreen){
+					GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+					newEnemy.transform.position = spawnPoint.transform.position;
+					enemiesOnScreen+=1;
+				}
+			}
+
+		}
+
+	}
+	public void removeEnemyFromScreen(){
+		if (enemiesOnScreen>0){
+			enemiesOnScreen-=1;
+		}
+	}
+}
