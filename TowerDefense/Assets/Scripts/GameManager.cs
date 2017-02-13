89f6a4aa9429a7d8a019e81e23b789b804afe62a
atapardea <atapardea@gmail.com>
2017-02-13 23:02:42 +0200

    added 8 enemies spawn

--- TowerDefense/Assets/Scripts/GameManager.cs
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour {
 	public int enemiesPerSpawin;
 
 	private int enemiesOnScreen=0;
+	const float spawnDelay=0.8f;
 
 	void Awake(){
 		if (instance==null)
@@ -25,25 +26,41 @@ public class GameManager : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		spawnEnemy();
+		StartCoroutine(spawn());
 	}
-	
-	// Update is called once per frame
-	
 
-	void spawnEnemy(){
+	IEnumerator spawn(){
 		if (enemiesPerSpawin>=0 && enemiesOnScreen < totalEnemies){
 			for (int i=0; i<enemiesPerSpawin ; i++){
 				if (enemiesOnScreen < maxEnemiesOnScreen){
-					GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+					GameObject newEnemy = Instantiate(enemies[2]) as GameObject;
 					newEnemy.transform.position = spawnPoint.transform.position;
 					enemiesOnScreen+=1;
 				}
 			}
-
+			yield return new WaitForSeconds(spawnDelay);
+			StartCoroutine(spawn());
 		}
 
 	}
+	
+	
+	// Update is called once per frame
+	
+
+	// void spawnEnemy(){
+	// 	if (enemiesPerSpawin>=0 && enemiesOnScreen < totalEnemies){
+	// 		for (int i=0; i<enemiesPerSpawin ; i++){
+	// 			if (enemiesOnScreen < maxEnemiesOnScreen){
+	// 				GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+	// 				newEnemy.transform.position = spawnPoint.transform.position;
+	// 				enemiesOnScreen+=1;
+	// 			}
+	// 		}
+
+	// 	}
+
+	// }
 	public void removeEnemyFromScreen(){
 		if (enemiesOnScreen>0){
 			enemiesOnScreen-=1;
